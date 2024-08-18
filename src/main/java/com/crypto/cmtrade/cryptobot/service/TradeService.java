be1e11ef021fced579130307cdcac574e6a8a7be
gh-ahuja <ghahuja@gmail.com>
2024-08-18 23:01:18 +0530

    Initial commit

---
+++ src/main/java/com/crypto/cmtrade/cryptobot/service/TradeService.java
@@ -0,0 +1,16 @@
+package com.crypto.cmtrade.cryptobot.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class TradeService {
+
+    @Scheduled(fixedDelay = 10000)
+    public void executeTrade(){
+        log.info("starting");
+
+    }
+}
---
+++ src/test/java/com/crypto/cmtrade/cryptobot/CryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.cmtrade.cryptobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
