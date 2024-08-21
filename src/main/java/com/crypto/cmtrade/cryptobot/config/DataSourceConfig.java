e417f6916e9b732ca331cdde95603aa866cdb695
gh-ahuja <ghahuja@gmail.com>
2024-08-21 12:27:04 +0530

    Tested select database after making changes to the database

--- src/main/java/com/crypto/cmtrade/cryptobot/config/DataSourceConfig.java
+++ src/main/java/com/crypto/cmtrade/cryptobot/config/DataSourceConfig.java
@@ -1,20 +1,37 @@
 package com.crypto.cmtrade.cryptobot.config;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+@Log4j2
 @Configuration
 public class DataSourceConfig {
-    
+
+    @Value("${database.jdbcUrl}")
+    private String jdbcUrl;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
+
+    @Value("${database.schema}")
+    private String schema;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("${jdbcUrl}");
-        dataSource.setUsername("${username}");
-        dataSource.setPassword("${password}");
+        log.info("system url " +jdbcUrl);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        log.info("password" +password);
+        dataSource.setPassword(password);
+        dataSource.setSchema(schema);
         return dataSource;
     }
 }
--- src/main/java/com/crypto/cmtrade/cryptobot/service/TradeService.java
+++ src/main/java/com/crypto/cmtrade/cryptobot/service/TradeService.java
@@ -1,18 +1,27 @@
 package com.crypto.cmtrade.cryptobot.service;
 
+import com.crypto.cmtrade.cryptobot.model.BatchTransaction;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Log4j2
 @Service
 public class TradeService {
 
-
+    @Autowired
+    private BatchTransactionService batchTransactionService;
 
     @Scheduled(fixedDelay = 10000)
     public void executeTrade(){
-        log.info("starting");
+
+        List<BatchTransaction> batchTransactions = batchTransactionService.getAllBatchTransactions();
+        for (BatchTransaction batchTransaction : batchTransactions){
+            log.info("batch transaction: " + batchTransaction);
+        }
 
     }
 }
